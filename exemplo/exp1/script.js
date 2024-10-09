cb8f29be3d4b5e17cc1eeeb0c2f23537be158893
chrystianmbarbosa <chrystian.mbarbosa@senacsp.edu.br>
2024-10-09 19:56:54 -0300

    feat: Exemplo 1 inserido

---
+++ exemplo/exp1/script.js
@@ -0,0 +1,16 @@
+
+/*Crie um algoritmo que leia um vetor de 5 números inteiros e mostre-os.*/
+
+let numeros = [];
+for (let i = 0; i < 5; i++) {
+
+    let numero = prompt(`Insira o ${i + 1} número!`);
+    while (isNaN(numero)) {
+        alert("Isso não é um número válido!");
+        numero = prompt("Insira um número novamente!");
+    }
+    numero = parseInt(numero);
+    // Inserindo o numero digitado ao vetor
+    numeros.push(numero);
+}
+console.log(numeros)
